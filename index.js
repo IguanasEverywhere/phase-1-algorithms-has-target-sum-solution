5abd9ea52aed472aa027560952a9b37c5d258817
Scott Schwab <scottschwab86@gmail.com>
2023-04-24 15:08:38 -0500

    Complete with linear time complexity

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    let obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let complement = target - array[i];
+    if (obj[complement]) {
+      return true;
+    } else {
+      obj[array[i]] = 1;
+    }
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
+  O(n), since we now only iterate over the array once, and accesing
+  keys on the object happens in constant time. We do create one new
+  object, so the space complexity is slightly worse, but that object
+  still grows linearly with the array, so it's still O(n) space complexity
 */
 
-/* 
+/*
   Add your pseudocode here
+
+  have an empty object
+  iterate over array
+    for each element, declare a complement that adds up to target
+    check if object has key of complement
+      if it does, return true
+    create/update key in object of current array value
+
+  if loop finishes, return false
 */
 
 /*
   Add written explanation of your solution here
+
+  This is more time efficient than the nested array, because we are only
+  iterating over the array once, and looking up the key in the object
+  happens in constant time. Space complexity is slightly worse, because
+  we make a new object, but it is still O(n)
 */
 
 // You can run `node index.js` to view these console logs
